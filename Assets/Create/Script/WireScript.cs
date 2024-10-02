2c77cff5e5f5ebc29d88fe8de05cb81e94af04e7
katsuyama-youhei <K022G0044@g.neec.ac.jp>
2024-10-02 16:31:29 +0900

    コメントアウトの削除

--- Assets/Create/Script/WireScript.cs
+++ Assets/Create/Script/WireScript.cs
@@ -57,7 +57,6 @@ public class WireScript : MonoBehaviour
     void ShootWire()
     {
         RaycastHit hit;
-        // var direction = transform.rotation * Vector3.forward;
 
         Vector3 rayDirection = transform.forward;
         Vector3 rayTransformPosition = new Vector3(transform.position.x, transform.position.y + 0.7f, transform.position.z);
@@ -66,25 +65,11 @@ public class WireScript : MonoBehaviour
 
         Debug.Log("Raycast start position: " + rayTransformPosition);
 
-      /*  var forward = transform.forward;
-
-        var right = transform.right;
-
-        // Vector3.forwardに姿勢のクォータニオンを掛ける
-        var forward2 = transform.rotation * Vector3.forward;
-
-        // Vector3.forwardをローカル座標系からワールド座標系に変換
-        var forward3 = transform.TransformDirection(Vector3.forward);
-
-        Debug.Log($"1:{forward}, 2:{forward2}, 3:{forward3}");
-        Debug.Log("right" + right);*/
-
         if (Physics.Raycast(rayTransformPosition, transform.forward, out hit, wireRange, grappleableLayers))
         {
             grapplePoint = hit.point;
             isGrappling = true;
             lineRenderer.enabled = true;
-            Debug.Log("AAA");
         }
         else
         {
